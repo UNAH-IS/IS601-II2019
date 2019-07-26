c5b579827b77000c9ee77b1802525165c4d90bb1
Erick Marín <erick.marin.v@gmail.com>
2019-07-25 23:33:09 -0600

    Ejercicio Cursor

---
+++ UnidadI/Ejercicios/ejercicios_fb_cursores.sql
@@ -0,0 +1,19 @@
+---Elaborar una procedimiento que imprima una cadena en formato JSON para todos los uaurios
+--con la siguiente estructura
+/*
+
+{
+  nombre:"Juan Perez",
+  genero:"M",
+  amigos:[
+    {nombre:"Pedro",edad:33},
+    {nombre:"Maria",edad:33},
+    {nombre:"Luis",edad:33}
+  ]
+}
+
+{nombre:"Juan Perez",genero:"M",amigos:[{nombre:"Pedro",edad:33},{nombre:"Maria",edad:33},{nombre:"Luis",edad:33}]}
+
+
+*/
+
